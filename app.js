e0134c79850899eec63c0dc454a22b69d8093be8
YuriiTrofimenko <tyaa@ukr.net>
2020-07-20 13:56:11 +0300

    js closure demo

--- app.js
+++ app.js
@@ -25,7 +25,28 @@ console.log(user2) */
 // const info = 
 // info.url = 'https://www.itbox.ua/api/v1/categories/Holodilniki-c7874/view=table;filter=1-00010000000'
 // info.method = 'GET'
-fetch('https://www.itbox.ua/api/v1/categories/Holodilniki-c7874/view=table;filter=1-00010000000')
+/* fetch('https://www.itbox.ua/api/v1/categories/Holodilniki-c7874/view=table;filter=1-00010000000')
   .then((response) => {
     console.log(response)
-  })
+  }) */
+
+// Замыкание - явление доступа функций высших порядков
+// к контексту функций первого порядка, которые их породили.
+// Функция высшего порядка - такая, которую возвращает (порождает) другая функция
+// при помощи слова return.
+// Контекст - это множество всех переменных и констант,
+// объявленных в теле функции.
+
+function parentFoo(startValue) {
+  const closure = {
+    'counter': startValue
+  }
+  return function (param) {
+    console.log(`Param = ${param} (call #${++closure.counter})`)
+  }
+}
+
+const childFoo = parentFoo(100)
+childFoo('One')
+childFoo(2)
+childFoo(3)
